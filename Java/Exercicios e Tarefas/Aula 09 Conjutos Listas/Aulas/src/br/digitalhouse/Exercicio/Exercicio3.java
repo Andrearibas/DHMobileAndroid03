52e8b5fe402b5e900dd49363b96e90f91d5c0be1
andrearibas <Andrearibas>
2019-08-13 22:15:14 -0300

    Inclusao aula 09, mais exercicios

--- Java/Exercicios e Tarefas/Aula 09 Conjutos Listas/Aulas/src/br/digitalhouse/Exercicio/Exercicio3.java
+++ Java/Exercicios e Tarefas/Aula 09 Conjutos Listas/Aulas/src/br/digitalhouse/Exercicio/Exercicio3.java
@@ -1,4 +1,24 @@
 package br.digitalhouse.Exercicio;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Exercicio3 {
+    public static void main(String[] args) {
+        Prova prova = new Prova();
+
+        Set<Integer> numeros = new HashSet<>();
+        numeros.add(4);
+        numeros.add(6);
+        numeros.add(12);
+        numeros.add(7);
+        numeros.add(9);
+        numeros.add(2);
+
+        prova.somaTotal(numeros);
+
+
+    }
 }
--- Java/Exercicios e Tarefas/Aula 09 Conjutos Listas/Aulas/src/br/digitalhouse/Exercicio/Prova.java
+++ Java/Exercicios e Tarefas/Aula 09 Conjutos Listas/Aulas/src/br/digitalhouse/Exercicio/Prova.java
@@ -3,12 +3,14 @@ package br.digitalhouse.Exercicio;
 import java.util.Set;
 
 public class Prova {
-    private float soma = 0;
 
     public void somaTotal(Set<Integer> conjuntoDeInteiros) {
-        for (int i = 0; i < conjuntoDeInteiros.size(); i++) {
-            soma = soma + i;
+        Integer somaConjuntos = 0;
+        for (Integer numero:conjuntoDeInteiros) {
+            somaConjuntos = somaConjuntos + numero;
+
         }
+        System.out.println("A soma do conjuto de numeros é: "+somaConjuntos);
     }
 
 }
