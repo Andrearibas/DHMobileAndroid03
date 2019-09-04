0d076f29ed83143ca4cafe43f0cc90555fdaa52a
andrearibas <Andrearibas>
2019-09-03 22:25:59 -0300

    Inclusao Aula de Layout

---
+++ Android/Layout/app/src/main/java/com/example/layout/RalativeLayout.java
@@ -0,0 +1,14 @@
+package com.example.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RalativeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ralative_layout);
+    }
+}
