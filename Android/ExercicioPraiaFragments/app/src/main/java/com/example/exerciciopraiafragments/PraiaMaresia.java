bf3f1da395430897149d6ee2f03c85086cee8d57
andrearibas <Andrearibas>
2019-09-17 22:24:59 -0300

    Inclusão aula Fragments

---
+++ Android/ExercicioPraiaFragments/app/src/main/java/com/example/exerciciopraiafragments/PraiaMaresia.java
@@ -0,0 +1,31 @@
+package com.example.exerciciopraiafragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PraiaMaresia extends Fragment {
+
+
+    public PraiaMaresia() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_praia_maresia, container, false);
+    }
+
+}
